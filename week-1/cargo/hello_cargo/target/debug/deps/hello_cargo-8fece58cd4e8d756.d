8fcdfb350a7bb7881870483304709f848c7d702e
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-05-15 21:09:07 -0600

    chore(git): ignore Rust build artifacts and IDE metadata via .gitignore

--- week-1/cargo/hello_cargo/target/debug/deps/hello_cargo-8fece58cd4e8d756.d
+++
@@ -1,5 +0,0 @@
-/Users/vatsalchovatiya55gmail.com/Desktop/workspace/code/learning_rust/week-1/cargo/hello_cargo/target/debug/deps/hello_cargo-8fece58cd4e8d756.d: src/main.rs
-
-/Users/vatsalchovatiya55gmail.com/Desktop/workspace/code/learning_rust/week-1/cargo/hello_cargo/target/debug/deps/hello_cargo-8fece58cd4e8d756: src/main.rs
-
-src/main.rs:
