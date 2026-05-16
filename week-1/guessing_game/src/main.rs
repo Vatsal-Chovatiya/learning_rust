7276b69a374bebb127bc5dc4e9b689ddfef2380c
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-05-16 00:06:47 -0600

    learn(guessing-game): handle invalid parsing errors gracefully with match

--- week-1/guessing_game/src/main.rs
+++ week-1/guessing_game/src/main.rs
@@ -1,13 +1,13 @@
-use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
+
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
     loop {
         println!("Please input your guess.");
 
@@ -17,13 +17,16 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too Big!"),
+            Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
                 println!("You win!");
                 break;
