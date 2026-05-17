6dbf575963e0665140384545f349c662a6eda417
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-05-16 21:08:33 -0600

    learn(types): define tuples and access elements using dot notation

--- week-1/Saturday/variables/src/main.rs
+++ week-1/Saturday/variables/src/main.rs
@@ -1,12 +1,13 @@
 fn main() {
-    let x = 5;
+    // Example of a tuple
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
 
-    let x = x + 1;
+    // Can destructure the tuple or acess the value of it by using (.) operator.
+    let x: (i32, f64, u8) = (500, 6.4, 1);
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
+    let five_hundred = x.0;
 
-    println!("The value of x is: {x}");
+    let six_point_four = x.1;
+
+    let one = x.2;
 }
