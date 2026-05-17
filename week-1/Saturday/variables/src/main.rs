56de3626e4d6063f538a561f7384275ee6141030
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-05-16 20:46:13 -0600

    learn(variables): declare constants using the const keyword

--- week-1/Saturday/variables/src/main.rs
+++ week-1/Saturday/variables/src/main.rs
@@ -1,7 +1,13 @@
 fn main(){
+
+    // Example of a mutable variable.
     let mut x = 5;
     println!("The value of x is: {x}");
 
     x = 6;
     println!("The value of x is: {x}");
+
+    // Example of a constant variable.
+    // Constants always in uppercase with underscore in between.
+    const THREE_HOURS_IN_SECONDS:u32 = 60 * 60 * 3;
 }
