de459b4d81a5b7d64eabd2f55990627c2c0762a7
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-05-16 22:37:17 -0600

    learn(control-flow): return values from loop expressions using break

--- week-1/Saturday/branches/src/main.rs
+++ week-1/Saturday/branches/src/main.rs
@@ -3,4 +3,17 @@ fn main() {
     let number = if condition { 5 } else { 6 };
 
     println!("The value of number is: {number}");
+
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
 }
