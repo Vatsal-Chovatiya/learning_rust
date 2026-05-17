fd98d72d1d6d8553f506092cc2445a96bb242c70
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-05-16 20:51:40 -0600

    learn(variables): shadow variables and explore inner scope shadowing

--- week-1/Saturday/variables/src/main.rs
+++ week-1/Saturday/variables/src/main.rs
@@ -1,13 +1,12 @@
-fn main(){
+fn main() {
+    let x = 5;
 
-    // Example of a mutable variable.
-    let mut x = 5;
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
     println!("The value of x is: {x}");
-
-    x = 6;
-    println!("The value of x is: {x}");
-
-    // Example of a constant variable.
-    // Constants always in uppercase with underscore in between.
-    const THREE_HOURS_IN_SECONDS:u32 = 60 * 60 * 3;
 }
