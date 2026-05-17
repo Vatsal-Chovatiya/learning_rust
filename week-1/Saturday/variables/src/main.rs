02991fc6fada199f6634b8b0681110672576eeed
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-05-16 21:16:04 -0600

    learn(types): define arrays, declare explicit array signatures, and index elements

--- week-1/Saturday/variables/src/main.rs
+++ week-1/Saturday/variables/src/main.rs
@@ -1,13 +1,10 @@
 fn main() {
-    // Example of a tuple
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    // Example of an array
+    let a = [1, 2, 3, 4, 5];
 
-    // Can destructure the tuple or acess the value of it by using (.) operator.
-    let x: (i32, f64, u8) = (500, 6.4, 1);
+    // Example of array where we assign the type of what kind it is and how many elements it is going to store
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
 
-    let five_hundred = x.0;
-
-    let six_point_four = x.1;
-
-    let one = x.2;
+    let first = a[0];
+    let second = a[1];
 }
