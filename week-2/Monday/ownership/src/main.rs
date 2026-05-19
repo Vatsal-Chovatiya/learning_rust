82b387f7a6f9ebe4ba966b0c1d5b3fa8696183dd
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-05-18 22:46:31 -0600

    learn(ownership): reassign String variable to trigger drop on old memory

--- week-2/Monday/ownership/src/main.rs
+++ week-2/Monday/ownership/src/main.rs
@@ -1,12 +1,6 @@
 fn main() {
     let mut s = String::from("hello");
+    s = String::from("ahoy");
 
-    s.push_str(", world!"); // push_str() appends a literal to a String
-
-    println!("{s}"); // this will print `hello, world!`
-
-    let s1 = String::from("hello");
-    let s2 = s1;
-
-    println!("{s2}, world!");
+    println!("{s}, world!");
 }
