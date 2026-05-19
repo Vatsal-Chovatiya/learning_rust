309ea9144f1eba43cb4c3fedc5e595727e19e8fc
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-05-19 13:40:35 -0600

    learn(ownership): clone heap data to perform a deep copy using clone

--- week-2/Monday/ownership/src/main.rs
+++ week-2/Monday/ownership/src/main.rs
@@ -3,4 +3,10 @@ fn main() {
     s = String::from("ahoy");
 
     println!("{s}, world!");
+    
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    println!("s1 = {s1}, s2 = {s2}");
+
 }
