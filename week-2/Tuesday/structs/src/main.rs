674303e6b665a78dab1f9b523473e511ca12ef2e
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-05-19 21:47:04 -0600

    learn(struct): Using debug mode to print the whole struct

--- week-2/Tuesday/structs/src/main.rs
+++ week-2/Tuesday/structs/src/main.rs
@@ -1,20 +1,10 @@
+#[derive(Debug)] // This makes it printable!
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1) // Passing a reference (borrowing)
-    );
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    let rect1 = Rectangle { width: 30, height: 50 };
+    println!("rect1 is {rect1:#?}");
 }
