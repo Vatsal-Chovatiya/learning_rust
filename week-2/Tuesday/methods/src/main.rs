a4526c68305092b19b016028eaca849f9e24d118
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-05-19 22:49:08 -0600

    learn(methods): Using associated functions to create new instances which does not takes self as an argument

--- week-2/Tuesday/methods/src/main.rs
+++ week-2/Tuesday/methods/src/main.rs
@@ -1,20 +1,39 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
+// 1. Define the data structure
+struct Player {
+    name: String,
+    level: u32,
 }
 
-// 1. Implementation block houses the methods
-impl Rectangle {
-    // 2. The first parameter must be self
-    fn area(&self) -> u32 {
-        self.width * self.height
+// 2. Implementation block (the tool chest for Player)
+impl Player {
+    
+    // --- ASSOCIATED FUNCTION (Constructor) ---
+    // It doesn't use "self" because a specific player doesn't exist yet. 
+    // It builds and returns a brand-new Player instance.
+    fn new(username: &str) -> Self {
+        Self {
+            name: username.to_string(),
+            level: 1, // Every new player starts at level 1
+        }
+    }
+
+    // --- METHOD ---
+    // It uses "&mut self" because it needs to modify a specific player's data.
+    fn level_up(&mut self) {
+        self.level += 1;
+        println!("🎉 {} leveled up to Level {}!", self.name, self.level);
     }
 }
 
+// 3. Run the program
 fn main() {
-    let rect1 = Rectangle { width: 30, height: 50 };
+    // Call the Associated Function using :: syntax
+    // This talks directly to the "Player" type to manufacture an instance.
+    let mut gamer = Player::new("ShadowBlade");
 
-    // 3. Methods are called using dot notation
-    println!("The area of the rectangle is {} square pixels.", rect1.area());
+    println!("Welcome {}, you are currently Level {}.", gamer.name, gamer.level);
+
+    // Call the Method using . syntax
+    // This tells our specific "gamer" instance to perform an action on itself.
+    gamer.level_up();
 }
