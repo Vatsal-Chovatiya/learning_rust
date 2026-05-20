ec7d26da950c2227fbc215222361133a19853bec
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-05-19 18:04:32 -0600

    learn(struct): An example on using tuple struct

--- week-2/Tuesday/structs/src/main.rs
+++ week-2/Tuesday/structs/src/main.rs
@@ -1,31 +1,7 @@
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
 
 fn main() {
-    // Convert &str literals to String
-    let mut user1 = build_user(
-        String::from("someexample@gmail.com"),
-        String::from("someone"),
-    );
-
-     let user2 = User {
-        email: String::from("another@example.com"),
-        ..user1
-    };
-    
-    // Silence the unused variable warning by doing something with it
-    println!("User email: {}", user1.email);
-}
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        active: true,
-        username,
-        email,
-        sign_in_count: 1,
-    }
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 }
