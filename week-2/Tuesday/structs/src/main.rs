2871c0c0c4aef044f4e00a87337b25fc10f1f918
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-05-19 21:51:48 -0600

    learn(struct): Using dbg! debugging tool to print and return its value and the output goes to stderr instead of stdout stream

--- week-2/Tuesday/structs/src/main.rs
+++ week-2/Tuesday/structs/src/main.rs
@@ -1,10 +1,15 @@
-#[derive(Debug)] // This makes it printable!
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
 fn main() {
-    let rect1 = Rectangle { width: 30, height: 50 };
-    println!("rect1 is {rect1:#?}");
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale), // Prints the math, then assigns 60 to width
+        height: 50,
+    };
+
+    dbg!(&rect1); // Pass a reference so dbg! doesn't steal ownership
 }
