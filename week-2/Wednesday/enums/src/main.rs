3bcd9d851b4cbb8a076773909df7c471a63cc91c
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-05-20 20:59:40 -0600

    learn(enum): Can insert any type of data in enum, custom struct, String, Integer etc

--- week-2/Wednesday/enums/src/main.rs
+++ week-2/Wednesday/enums/src/main.rs
@@ -1,7 +1,12 @@
-enum IpAddr {
-    V4(String),
-    V6(String),
+struct Ipv4Addr {
+    // --snip--
 }
 
-let home = IpAddr::V4(String::from("127.0.0.1"));
-let loopback = IpAddr::V6(String::from("::1"));
+struct Ipv6Addr {
+    // --snip--
+}
+
+enum IpAddr {
+    V4(Ipv4Addr),
+    V6(Ipv6Addr),
+}
