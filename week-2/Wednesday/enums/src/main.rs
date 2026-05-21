4566cfacf1ff670ebc46a5bb225611c806063dfe
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-05-20 20:55:17 -0600

    learn(enum): Combining enums and struct for storing data

--- week-2/Wednesday/enums/src/main.rs
+++ week-2/Wednesday/enums/src/main.rs
@@ -3,12 +3,17 @@ enum IpAddrKind {
     V6,
 }
 
-let four = IpAddrKind::V4;
-let six = IpAddrKind::V6;
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
 
+let home = IpAddr {
+    kind: IpAddrKind::V4,
+    address: String::from("127.0.0.1"),
+};
 
-fn route(ip_kind: IpAddrKind) {}
-
-// You can call it with either variant
-route(IpAddrKind::V4);
-route(IpAddrKind::V6);
+let loopback = IpAddr {
+    kind: IpAddrKind::V6,
+    address: String::from("::1"),
+};
