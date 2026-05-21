159bed42233fb4f55828436f090cca0236d8aa80
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-05-20 22:10:04 -0600

    learn(match): Used for the matching control flow

--- week-2/Wednesday/enums/src/main.rs
+++ week-2/Wednesday/enums/src/main.rs
@@ -1,15 +1,15 @@
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
 }
 
-impl Message {
-    fn call(&self) {
-        // method body would be defined here
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
     }
 }
-
-let m = Message::Write(String::from("hello"));
-m.call();
