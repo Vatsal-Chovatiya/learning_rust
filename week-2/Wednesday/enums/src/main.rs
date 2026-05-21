de446425308213e678a44f3382a00740d5e15957
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-05-20 22:11:16 -0600

    Last commit's v2

--- week-2/Wednesday/enums/src/main.rs
+++ week-2/Wednesday/enums/src/main.rs
@@ -4,10 +4,12 @@ enum Coin {
     Dime,
     Quarter,
 }
-
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1 // The last line determines the return value
+        }
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter => 25,
