d4165c5bb118950b8e058353c3a6f9734b2b4ce2
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-05-20 20:53:13 -0600

    learn(enum): creating instance of an enum variants

--- week-2/Wednesday/enums/src/main.rs
+++ week-2/Wednesday/enums/src/main.rs
@@ -2,3 +2,13 @@ enum IpAddrKind {
     V4,
     V6,
 }
+
+let four = IpAddrKind::V4;
+let six = IpAddrKind::V6;
+
+
+fn route(ip_kind: IpAddrKind) {}
+
+// You can call it with either variant
+route(IpAddrKind::V4);
+route(IpAddrKind::V6);
