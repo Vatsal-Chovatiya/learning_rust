b68728a375dc10f0f66a5ff38312e2922202a768
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-05-26 21:09:20 -0600

    learn(use): An example of a hashmap library using idiomatic way import

--- week-3/Tuesday/project/src/main.rs
+++ week-3/Tuesday/project/src/main.rs
@@ -1,11 +1,6 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-    }
-}
+use std::collections::HashMap;
 
-use crate::front_of_house::hosting::add_to_waitlist;
-
-pub fn eat_at_restaurant() {
-    add_to_waitlist();
+fn main() {
+    let mut map = HashMap::new();
+    map.insert(1, 2);
 }
