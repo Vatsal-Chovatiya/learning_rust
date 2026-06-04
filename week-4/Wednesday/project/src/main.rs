258d7c9768f0b542ce71eae25954b23543ed1f99
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-06-03 23:50:28 -0600

    learn(HashMap): Overwriting a value in a HashMap

---
+++ week-4/Wednesday/project/src/main.rs
@@ -0,0 +1,8 @@
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+
+    println!("{scores:?}");
