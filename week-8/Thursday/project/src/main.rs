1799ac02f6d826b23c9a0a9c8e73e0d13f272416
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-07-02 07:56:40 -0600

    learn(traits): Using Multiple trait bounds with the + syntax

--- week-8/Thursday/project/src/main.rs
+++ week-8/Thursday/project/src/main.rs
@@ -3,6 +3,10 @@
 // }
 
 
-pub fn notify<T: Summary>(item: &T) {
-    println!("Breaking news! {}", item.summarize());
-}
+// pub fn notify<T: Summary>(item: &T) {
+//     println!("Breaking news! {}", item.summarize());
+// }
+
+pub fn notify(item: &(impl Summary + Display)) {}
+
+pub fn notify<T: Summary + Display>(item: &T) {}
