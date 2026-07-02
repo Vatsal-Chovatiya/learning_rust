fe5cd39791f4b05ed550399583d5a7d50a1d9bad
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-07-02 07:49:07 -0600

    learn(traits): Trait bound syntax of the same thing

--- week-8/Thursday/project/src/main.rs
+++ week-8/Thursday/project/src/main.rs
@@ -1,3 +1,8 @@
-pub fn notify(item: &impl Summary) {
+// pub fn notify(item: &impl Summary) {
+//     println!("Breaking news! {}", item.summarize());
+// }
+
+
+pub fn notify<T: Summary>(item: &T) {
     println!("Breaking news! {}", item.summarize());
 }
