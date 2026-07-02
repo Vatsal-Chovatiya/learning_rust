49b0aab275a22349a2f9226b60589047d6e4c6b3
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-07-02 07:31:21 -0600

    learn(traits): This is how we use default signature method in main function

--- week-8/Thursday/project/src/main.rs
+++ week-8/Thursday/project/src/main.rs
@@ -4,3 +4,21 @@ pub trait Summary {
         String::from("(Read more...)")
     }
 }
+
+// An empty impl block means we are using the default summarize method
+impl Summary for NewsArticle {}
+
+fn main() {
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
+    };
+
+    // We can still call summarize, even though we didn't write it for NewsArticle
+    println!("New article available! {}", article.summarize());
+}
