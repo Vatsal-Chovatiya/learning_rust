037bb094f8697dc26ba1d9986c3738c4569739b2
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-07-02 07:58:44 -0600

    learn(traits): Use where clause to wrap trait bound inside it

--- week-8/Thursday/project/src/main.rs
+++ week-8/Thursday/project/src/main.rs
@@ -10,3 +10,10 @@
 pub fn notify(item: &(impl Summary + Display)) {}
 
 pub fn notify<T: Summary + Display>(item: &T) {}
+
+
+fn some_function<T, U>(t: &T, u: &U) -> i32
+where
+    T: Display + Clone,
+    U: Clone + Debug,
+{}
