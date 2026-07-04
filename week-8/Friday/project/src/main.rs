1ae29fcbebd1472182b65bd30753668608d1c785
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-07-03 23:07:31 -0600

    learn(traits): Returning Types That Implement Traits

---
+++ week-8/Friday/project/src/main.rs
@@ -0,0 +1,10 @@
+fn returns_summarizable() -> impl Summary {
+    SocialPost {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        repost: false,
+    }
+}
