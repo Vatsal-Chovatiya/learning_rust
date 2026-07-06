597c910c9fef229e016c55cdad919abefea299a7
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-07-06 11:15:30 -0600

    learn(lifetimes): Using annotation and checking it

--- week-9/Monday/project/src/main.rs
+++ week-9/Monday/project/src/main.rs
@@ -1,10 +1,8 @@
 fn main() {
-    let r;                // 1. Outer scope: declare 'r' without an initial value
-
-    {                     // 2. Inner scope starts
-        let x = 5;        // 3. Declare 'x' and give it the initial value of 5
-        r = &x;           // 4. Attempt to set 'r' as a reference to 'x'
-    }                     // 5. Inner scope ends: 'x' goes out of scope here
-
-    println!("r: {r}");   // 6. Outer scope: attempt to print the value in 'r'
-}
+    let x = 5;            // ----------+-- 'b (Lifetime of x)
+                          //           |
+    let r = &x;           // --+-- 'a  | (Lifetime of r)
+                          //   |       |
+    println!("r: {r}");   //   |       |
+                          // --+       |
+}                         // ----------+
