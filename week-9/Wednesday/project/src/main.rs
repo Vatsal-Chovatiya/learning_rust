7ca88671c0bed5b114d935a56ced55c9012895b9
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-07-08 21:16:37 -0600

    learn(lifetimes): Code example without lifetimes

---
+++ week-9/Wednesday/project/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let ans;
+
+    let str1 = String::from("Small");
+    {
+        let str2 = String::from("Longer");
+        ans = longest(str1, str2);
+    }
+    println!("{:?}", ans)
+}
+
+fn longest(a: String, b: String) -> String {
+    if a.len() > b.len() {
+        return a;
+    } else {
+        return b;
+    }
+}
