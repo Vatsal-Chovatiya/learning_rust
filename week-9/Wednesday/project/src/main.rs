33c6571720d0cfba6a0a1b4d27a18beae4553786
Vatsal-Chovatiya <vatsalchovatiya55@gmail.com>
2026-07-08 21:43:00 -0600

    learn(lifetimes): Code example with lifetime which shows better error

--- week-9/Wednesday/project/src/main.rs
+++ week-9/Wednesday/project/src/main.rs
@@ -4,12 +4,12 @@ fn main() {
     let str1 = String::from("Small");
     {
         let str2 = String::from("Longer");
-        ans = longest(str1, str2);
+        ans = longest(&str1, &str2);
     }
     println!("{:?}", ans)
 }
 
-fn longest(a: String, b: String) -> String {
+fn longest<'a>(a: &'a str, b: &'a str) -> &'a str {
     if a.len() > b.len() {
         return a;
     } else {
